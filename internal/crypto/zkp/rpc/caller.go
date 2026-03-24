3964e97ea59d3518589623efca9e0aa162b8c7fd
raaffs <suyash.saraf@somaiya.edu>
2026-03-24 21:25:16 +0530

    Implement persistent ZKProver service with gRPC keepalive
    - Moved gRPC client logic from main into a dedicated ZKProverClient struct.
    - Added google.golang.org/grpc/keepalive to maintain the connection during
    - This ensures the long-lived connection to the Rust prover doesn't
      time out during idle periods or long-running computations.

---
+++ internal/crypto/zkp/rpc/caller.go
@@ -0,0 +1,92 @@
+package rpc
+
+import (
+	"context"
+	"time"
+	pb "zkRPC/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+)
+type ZKProverClient struct {
+	client	pb.ProverServiceClient
+	conn	*grpc.ClientConn
+}
+
+func NewZKProverClient(endpoint string) (*ZKProverClient, error) {
+	kacp:= keepalive.ClientParameters{
+		Time: 					10*time.Second,
+		Timeout: 				2*time.Second,
+		PermitWithoutStream: 	true,
+	}
+
+	conn,err:=grpc.NewClient(
+		endpoint,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithKeepaliveParams(kacp),
+	)
+
+	if err!=nil{
+		return nil, err
+	}
+
+	return &ZKProverClient{
+		client:pb.NewProverServiceClient(conn), 
+		conn:conn,
+	}, nil
+}
+
+func (c *ZKProverClient) Close() {
+	c.conn.Close()
+}
+
+func (c *ZKProverClient) RequestMembershipProof(
+	ctx context.Context,
+	actualValue string,
+	actualSalt string,
+	allLeaves []string,
+	publicList []string,
+	publicRoot string,
+
+)(*pb.ProofResponse, error){
+	req := &pb.ProofRequest{
+		ProofData: &pb.ProofRequest_Membership{
+			Membership: &pb.MembershipRequest{
+				ActualValue: actualValue,
+				ActualSalt:  actualSalt,
+				AllLeaves:   allLeaves,
+				PublicList:  publicList,
+				PublicRoot:  publicRoot,
+			},
+		},
+	}
+
+	return c.client.GenerateProof(ctx, req)
+}
+
+func (c *ZKProverClient) RequestRangeProof(
+	ctx context.Context,
+	actualValue string,
+	actualSalt string,
+	allLeaves []string,
+	lowerBound uint32,
+	upperBound uint32,
+	publicRoot string,
+
+)(*pb.ProofResponse, error){
+	req := &pb.ProofRequest{
+		ProofData: &pb.ProofRequest_Range{
+			Range: &pb.RangeRequest{
+				ActualValue: actualValue,
+				ActualSalt:  actualSalt,
+				AllLeaves:   allLeaves,
+				LowerBound: lowerBound,
+				UpperBound: upperBound,
+				PublicRoot:  publicRoot,
+			},
+		},
+	}
+
+	return c.client.GenerateProof(ctx, req)
+}
